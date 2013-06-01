a065e467f8ac795aa28cfb1493ac97077b32f044
darkcoordinate <sagacious112358@gmail.com>
2013-06-01 17:13:23 +0530

    Update and rename hanoi.cpp to hanoi.h

--- hanoi.h
+++ hanoi.h
@@ -17,12 +17,12 @@ public:
         
     }
     
-    void addpoint(int k)
+    void addpoint(int i ,int k)
     {
         point^ p;
         for (p = list[k] ; p->j != nullptr ; p = p->j);
         point^ p1 = gcnew point();
-        p1->i = 0;
+        p1->i = i;
         p1->j = nullptr;
         p->j = p1;
         p1 = nullptr;
@@ -34,7 +34,19 @@ public:
     void movstak(int from , int to,)
     {
         point^ p1;
-        for(p1 = list[] = p1->j != nullptr; p = p->j);
-        
+        point^ p2;
+        point^ p3;
+        for(p1 = list[from]; p1->j != nullptr; p1 = p1->j);
+        for(p2 = list[from]; p2->j->j != nullptr; p2= p2->j);
+        for(p3 = list[to]; p3-> != nullptr; p3 = p3->j );
+        p3->j = p1;
+        p2->j = nullptr;
+        p1 = nullptr;
+        p2 = nullptr;
+        p3 = nullptr;
+        delete p1;
+        delete p2;
+        delete p3;
+    }   
 };
 
