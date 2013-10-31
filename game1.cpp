314bc8433f366a6ad1af98cd46e530acbaf48d23
darkcoordinate <sagacious112358@gmail.com>
2013-11-01 04:19:26 +0530

    Update game1.cpp

--- game1.cpp
+++ game1.cpp
@@ -1,3 +1,22 @@
 #include "game1.h"
 
+vector :: vector(double x, double y, double z)
+{
+  vx = x;
+  vy = y;
+  vz = z;
+}
 
+vector :: ~vector()
+{
+  
+}
+
+vector vector :: operator+(vector v1)
+{
+  vector v;
+  v.vx = vx + v1.vx;
+  v.vy = vy + v1.vy;
+  v.vz = vz + v1.vz;
+  retrun v;
+}
