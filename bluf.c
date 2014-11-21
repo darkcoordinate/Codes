b2eaba7af32122a28513d2d4f3aa20b6ace73f9b
darkcoordinate <sagacious112358@gmail.com>
2014-11-21 16:04:20 +0530

    Update bluf.c

--- bluf.c
+++ bluf.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+    int type;
+    int rank;
+} Card;
+
+typedef struct{
+    Card* card;
+    Card_list *next;
+    Card_list *prev;
+} Card_list;
+
+typedef struct{
+    Card_list *crd_lst;
+    Card_cllct *next;
+    Card_cllct *prev;
+} Card_cllct;
+
+void add_card(Card card, Card_list* crd_list)
+{
+    Card_list *p1, *p2;
+    p1 = crd
+}
 int main()
 {
+    
+}
